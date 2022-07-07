d7a36b7a94f89e76676ef45dd8d98f48f54b3796
Jimmy Kuo <kuo.yangyang@gmail.com>
2022-07-07 19:08:43 +0800

    Format all js files

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ if (!config || !config.port || !config.endpoint) process.exit();
 // Initialize Metrics Server
 console.log('Initializing Metrics...');
 const metrics = require('./lib/metrics').metrics;
-metrics.start(config.port,config.host,config.endpoint);
+metrics.start(config.port, config.host, config.endpoint);
 
-console.log('Initializing connection...',config.clickhouse.host);
+console.log('Initializing connection...', config.clickhouse.host);
 const click = require('./lib/click').click;
 click.start(config.interface);
 
